b76522c82aa41d46f8b1855ba64986f5ef45adf0
Mandelli <marco.mandelli@elmec.it>
2023-05-30 14:39:28 +0200

    Added support query timeout

--- health_check/db/backends.py
+++ health_check/db/backends.py
@@ -1,4 +1,6 @@
-from django.db import DatabaseError, IntegrityError
+import os
+
+from django.db import DatabaseError, IntegrityError, connection
 
 from health_check.backends import BaseHealthCheckBackend
 from health_check.exceptions import ServiceReturnedUnexpectedResult, ServiceUnavailable
@@ -7,13 +9,27 @@ from .models import TestModel
 
 
 class DatabaseBackend(BaseHealthCheckBackend):
+
+    def set_query_timeout(self, timeout, cursor):
+        engine = connection.settings_dict['ENGINE']
+        if 'postgresql' in engine:
+            cursor.execute(f"SET statement_timeout = '{timeout}s'")
+        elif 'mysql' in engine:
+            cursor.execute(f"SET SESSION max_statement_time = {timeout}")
+        else:
+            return None
+
+
     def check_status(self):
+        timeout = os.environ.get('QUERY_TIMEOUT', 2)
         try:
-            obj = TestModel.objects.create(title="test")
-            obj.title = "newtest"
-            obj.save()
-            obj.delete()
+            with connection.cursor() as cursor:
+                self.set_query_timeout(timeout, cursor)
+                obj = TestModel.objects.create(title="test")
+                obj.title = "newtest"
+                obj.save()
+                obj.delete()
         except IntegrityError:
             raise ServiceReturnedUnexpectedResult("Integrity Error")
-        except DatabaseError:
-            raise ServiceUnavailable("Database error")
+        except DatabaseError as e:
+            raise ServiceUnavailable("Database error {}".format(e))
